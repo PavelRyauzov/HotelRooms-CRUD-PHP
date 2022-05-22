80468bd4d387323859f6e3fa5dfb332f5c8635dc
Pavel Ryauzov <pavel.ryauzov@gmail.com>
2022-05-23 00:33:08 +0300

    Добавлен класс RoomTable

---
+++ .core/rooms/RoomTable.php
@@ -0,0 +1,70 @@
+<?php
+
+class RoomTable
+{
+    public static function create(string $imgPath, string $name, int $employeeId, string $desc, string $price)
+    {
+        $query = Database::prepare(
+            'INSERT INTO `rooms` (img_path, name, employee_id, description, price)' .
+            'VALUES (:imgPath, :name, :employeeId, :desc, :$price)');
+        $query->bindValue(':imgPath', $imgPath, PDO::PARAM_STR);
+        $query->bindValue(':name', $name, PDO::PARAM_STR);
+        $query->bindValue(':employeeId', $employeeId, PDO::PARAM_INT);
+        $query->bindValue(':desc', $desc, PDO::PARAM_STR);
+        $query->bindValue(':price', $price, PDO::PARAM_STR);
+
+        if (!$query->execute()) {
+            throw new PDOException('При добавлении гостиничного номера произошла ошибка');
+        }
+    }
+
+    public static function update(int $id, string $imgPath, string $name, int $employeeId, string $desc, string $price)
+    {
+        $query = Database::prepare('UPDATE `rooms` SET `$img_path` = :imgPath,' .
+            '`name` = :name, `employee_id` = :employeeId, `description` = :desc, `price` = :price WHERE `id` = :id');
+        $query->bindValue(":id", $id, PDO::PARAM_INT);
+        $query->bindValue(":imgPath", $imgPath, PDO::PARAM_STR);
+        $query->bindValue(':name', $name, PDO::PARAM_STR);
+        $query->bindValue(':employeeId', $employeeId, PDO::PARAM_INT);
+        $query->bindValue(':desc', $desc, PDO::PARAM_STR);
+        $query->bindValue(':price', $price, PDO::PARAM_STR);
+
+        if (!$query->execute()) {
+            throw new PDOException('При редактировании данных о гостиничном номере произошла ошибка');
+        }
+    }
+
+    public static function delete(int $id) {
+        $query = Database::prepare('DELETE FROM `rooms` WHERE `id`= :id');
+        $query->bindValue(":id", $id, PDO::PARAM_INT);
+
+        if (!$query->execute()) {
+            throw new PDOException('При удалении гостиничного номера произошла ошибка');
+        }
+    }
+
+    public static function getAll(): array
+    {
+        $query = Database::prepare(
+            'SELECT room.id, room.img_path, room.name, room.description, room.price, employee_id, employee.name as employee_name'.
+                      'FROM `rooms` JOIN `employees` ON employee_id = employee.id');
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
+    public static function getById(int $id): array
+    {
+        $query = Database::prepare('SELECT * FROM `rooms` WHERE `id` = :id LIMIT 1');
+        $query->bindValue(":id", $id, PDO::PARAM_INT);
+        $query->execute();
+
+        $data = $query->fetchAll();
+
+        if (!count($data)) {
+            return [];
+        }
+
+        return $data[0];
+    }
+}
